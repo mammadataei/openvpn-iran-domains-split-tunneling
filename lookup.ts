f3c0f29f66f198d1bdcc35869e425f9e1536d16b
Mohammad Ataei <mammadataei@gmail.com>
2022-09-06 07:28:13 +0430

    refactor: update lookup script
    [resolve_ips]

--- lookup.ts
+++ lookup.ts
@@ -1,20 +1,34 @@
-import { performDNSLookup } from "./utils.ts";
+import {
+  resolve4,
+  setServers,
+} from "https://deno.land/std@0.154.0/node/dns/promises.ts";
 
-console.log("Loading domains list from `domains.txt`...");
-const domains = (await Deno.readTextFile("domains.txt")).split("\n");
+setServers(["178.22.122.100"]);
 
-console.log("Performing DNS lookup, this will take a while...");
-const { resolvedIPs, unresolvedDomains } = await performDNSLookup(domains);
-
-console.log("Updating `resolvedIPs.txt`...");
-await Deno.writeTextFile("resolvedIPs.txt", resolvedIPs.join("\n"));
-
-console.log("Removing unresolved domains from `domains.txt`...");
-const updatedDomainsList = domains.filter(
-  (domain) => !unresolvedDomains.includes(domain)
+console.log("Loading domains list from `domains.json`...");
+const domains: Record<string, string[] | null> = JSON.parse(
+  await Deno.readTextFile("domains.json")
 );
 
-console.log("Updating `domains.txt`...");
-await Deno.writeTextFile("domains.txt", updatedDomainsList.join("\n"));
+console.log("Performing DNS lookup, this will take a while...");
+
+let counter = 0;
+
+for await (const domain of Object.keys(domains).slice(0, 2000)) {
+  if (domains[domain] === null) {
+    console.log(`Resolving "${domain}":`);
+
+    try {
+      const IPs = (await resolve4(domain)) as unknown as string[];
+      domains[domain] = IPs;
+      counter++;
+    } catch {
+      console.log(`Resolution for ${domain} failed.`);
+    }
+  }
+}
+
+console.log(`${counter} domains resolved. Updating "domains.json"...`);
+await Deno.writeTextFile("domains.json", JSON.stringify(domains, null, "  "));
 
 console.log("Done!");
